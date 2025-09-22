23138ed063385ceaf3b01ea47535f7a401362ccf
AlexT <Alex.Tom08@outlook.de>
2025-09-22 20:53:31 +0200

    Initial commit

---
+++ src/main/kotlin/de/alextom/pvprun/PVPRUN.kt
@@ -0,0 +1,14 @@
+package de.alextom.pvprun
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class PVPRUN : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
